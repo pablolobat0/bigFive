18a5ae585fe2dc9dbb4bc5486ae3eabc72ef7ecc
Pablo Seijo <pgarsei510@gmail.com>
2025-02-22 09:54:18 +0100

    HotFix: Arreglo del bug de la rama delvelop del Frontend. Enrutado corregido

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.tsx";
 import Chat from "./pages/Chat.tsx";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="bg-white min-h-screen w-full max-w-full overflow-x-hidden flex flex-col items-center">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="bg-white min-h-screen w-full max-w-full overflow-x-hidden flex flex-col items-center">
+    <Routes>
+      <Route path="/" element={<Navigate to="/inicio" />} />
+      <Route path="/inicio" element={<LandingPage />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  </div>
   );
 };
 
