19b9c9d1842523b51eca90e2f5207754a11edae8
Pablo Seijo <pgarsei510@gmail.com>
2025-02-22 03:05:34 +0100

    Eliminación de desbordamiento

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
-import Header from "./components/Header.tsx";
-import HeroSection from "./components/HeroSection.tsx";
-import QueOfrecemos from "./components/QueOfrecemos.tsx";
-import Pasos from "./components/Pasos.tsx";
-import Footer from "./components/Footer.tsx";
+import React from "react";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import QueOfrecemos from "./components/QueOfrecemos";
+import Pasos from "./components/Pasos";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="bg-white min-h-screen">
+    <div className="bg-white min-h-screen w-full max-w-full overflow-x-hidden flex flex-col items-center">
       <Header />
       <HeroSection />
       <QueOfrecemos />
