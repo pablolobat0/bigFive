af65ee9f5ed2408a5560b095f49c9f40ffa710d6
Pablo Seijo <105173542+pabloseijo@users.noreply.github.com>
2025-02-22 12:12:30 +0100

    Feature/diario (#14)
    * Primera versión funcional del diario
    
    * Desplazos corregidos en la nodelist y en el nodeeditor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.tsx";
 import Chat from "./pages/Chat.tsx";
+import Diario from "./pages/Diario.tsx";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Route path="/" element={<Navigate to="/inicio" />} />
       <Route path="/inicio" element={<LandingPage />} />
       <Route path="/chat" element={<Chat />} />
+      <Route path="/diario" element={<Diario />} />
     </Routes>
   </div>
   );
